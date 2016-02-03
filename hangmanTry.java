da65e05f98a4777c9fa0fdb268a5d93fbefb4aa4
yilmazkorkmazz <ylmaz1906@hotmail.com>
2016-02-03 21:35:12 +0200

    branche ekleme

---
+++ hangmanTry.java
@@ -0,0 +1,11 @@
+public class hangmanTry{
+  
+  public static void main( String [] args)
+  {
+    Hangman hang = new Hangman();
+    
+    System.out.println(hang.toString())
+      ;
+    System.out.println("asdadsfdsfg");
+  
+  }}
